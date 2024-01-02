da11051316b6aa67f56241ef9b5e69c7b5b39122
Ramin Tim Alizadeh Kashani <ramin.alizadeh.kashani@googlemail.com>
2024-01-02 10:47:39 +0100

    lsp

--- lua/hulla/plugins.lua
+++ lua/hulla/plugins.lua
@@ -4,17 +4,29 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+-- Packer can manage itself
+use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+use {
+	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	requires = { {'nvim-lua/plenary.nvim'} }
+}
 
-  use { "ellisonleao/gruvbox.nvim" }
+use { "ellisonleao/gruvbox.nvim" }
 
-  use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  end)
+use {
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v3.x',
+	requires = {
+		-- LSP Support
+		{'neovim/nvim-lspconfig'},
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'L3MON4D3/LuaSnip'},
+	}
+}
+
+end)
